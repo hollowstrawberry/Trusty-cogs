fff3fab08514282715ac4fd14e4901a6f2f04586
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2023-04-28 20:46:03 -0400

    Delete oof.py

--- oof.py
+++
@@ -1,8 +0,0 @@
-import os
-for folder in [f.path for f in os.scandir('.') if f.is_dir()][3:]:
-    with open(os.path.join(folder, "info.json"), 'r') as f:
-        contents = f.read()
-    contents = contents.replace('"3.5.0"', '"3.5.0.dev363"')
-    with open(os.path.join(folder, "info.json"), 'w') as f:
-        f.write(contents)
-    print(os.path.join(folder, "info.json"))
