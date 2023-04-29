0f1b348e467083f5a494630b97f455c46c43264e
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2023-04-28 20:03:36 -0400

    init

--- crabrave/__init__.py
+++ crabrave/__init__.py
@@ -1,12 +1,7 @@
-import json
-from pathlib import Path
-
 from .crabrave import CrabRave
+from redbot.core.utils import get_end_user_data_statement
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 async def setup(bot):
-    cog = CrabRave(bot)
-    await bot.add_cog(cog)
+    await bot.add_cog(CrabRave(bot))
